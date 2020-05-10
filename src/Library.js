e2d92f20ce485766d6b8b4ab9f0911b92f824681
Wattanit Hotrakool <wattanit.h@gmail.com>
2020-05-10 23:05:51 +0700

    Install PouchDB

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import PouchDB from 'pouchdb'
 
 class Library extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.db = new PouchDB('flashhive');
+        this.db.info().then((info)=>{
+            console.log(info)
+        })
+    }
     render() {
+        
         return (
             <div>
                 LIBRARY
